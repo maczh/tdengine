14d9e7bec83a7169131a70575c5992d25ea2e355
maczh <Micro!070804>
2022-09-30 13:38:14 +0800

    修改Debug输出

--- utils.go
+++ utils.go
@@ -66,7 +66,7 @@ func getValByTag(refVal reflect.Value, refType reflect.Type, tag string) (interf
 	//refType := reflect.TypeOf(obj).Elem()
 	for i := 0; i < refVal.NumField(); i++ {
 		field := refType.Field(i)
-		logger.Debug("field:" + field.Name + ",tag:" + tag + "tagval:" + field.Tag.Get("td"))
+		//logger.Debug("field:" + field.Name + ",tag:" + tag + "tagval:" + field.Tag.Get("td"))
 		if tag == field.Tag.Get("td") {
 			return refVal.Field(i).Interface(), field.Type.Kind(), nil
 		}
